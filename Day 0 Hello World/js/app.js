ac50fb5d0b3a8c9faffcf5cbd13a0c2fa36a00c9
Andrew Abalos <andysaurusrex21@gmail.com>
2017-10-03 08:44:07 -0700

    solved

--- Day 0 Hello World/js/app.js
+++ Day 0 Hello World/js/app.js
@@ -1,4 +1,7 @@
 function processData(inputString) {
+    // This line of code prints the first line of output
     console.log("Hello, World.");
-    console.log("Welcome to 30 Days of code!");
-}
+    
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+} 
